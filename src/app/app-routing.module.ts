eaca5bdf0db6e1fc8ce94207bf1520f6f54c6bbd
InDc12 <indfcr12@hotmail.com>
2023-08-17 15:16:10 -0500

    Add first Ionic changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:"full"
+  },
+  {
+    path : 'home' , 
+    component : HomeComponent
+  },
+  {
+    path :'login',
+    component: LoginComponent
+  },
+  {
+    path :'products',
+    component: ProductComponent
+  },
+  {
+    path:'productDetail',
+    component: ProductDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { IonicModule } from '@ionic/angular';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent,
+    LoginComponent,
+    ProductsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
